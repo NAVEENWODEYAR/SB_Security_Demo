890eee1798c96c4fbc66b95cdbe26000b97d2ed3
Naveen <naveenkrishnawodeyar@gmail.com>
2024-08-22 23:48:04 +0530

    initial commit, spring_security demo app

---
+++ src/main/java/com/security/asfalea/AsfaleaApplication.java
@@ -0,0 +1,13 @@
+package com.security.asfalea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsfaleaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsfaleaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/asfalea/AsfaleaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.asfalea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsfaleaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
