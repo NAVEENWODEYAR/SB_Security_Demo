06a0c0df372463e4bdf8b2fa1ba9ff7f2663d8fb
Naveen Kumar K <108418552+NAVEENWODEYAR@users.noreply.github.com>
2026-08-23 18:36:08 +0530

    Update AsfaleaApplication.java

--- src/main/java/com/security/asfalea/AsfaleaApplication.java
+++ src/main/java/com/security/asfalea/AsfaleaApplication.java
@@ -62,7 +62,7 @@ public class AsfaleaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AsfaleaApplication.class, args);
-		System.out.println("\nSPRING~^~SECURITY\n");
+		System.out.println("\nSPRING~^~SECURITY\n Demo Application");
 	}
 
 }
